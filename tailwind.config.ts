6f82a7e4bd9b10a109054072e76f7187a5926b1f
nethen <72537484+nethen@users.noreply.github.com>
2024-10-29 23:23:52 -0700

    feat: added screens up to 4k

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,12 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      screens: {
+        "2xl": "1440px",
+        "3xl": "1536px",
+        "4xl": "1920px",
+        "5xl": "2560px",
+      },
     },
   },
   plugins: [],
