bd954d4619369a4c3677657b7cd0c2a97429f533
nethen <72537484+nethen@users.noreply.github.com>
2024-11-24 02:57:23 -0800

    feat: added shift-g grid

--- src/app/components/Grid/Grid.tsx
+++ src/app/components/Grid/Grid.tsx
@@ -1,13 +1,23 @@
-import { useBoolean } from "usehooks-ts";
+"use client";
+import { motion } from "motion/react";
+import { useBoolean, useEventListener } from "usehooks-ts";
 
 export const Grid = () => {
   const visible = useBoolean(false);
+  useEventListener("keydown", (e) => {
+    if (e.shiftKey && e.key === "G") {
+      visible.toggle();
+    }
+  });
   return (
-    <div className="fixed inset-0 grid grid-cols-3 auto-rows-min md:grid-cols-6 lg:grid-cols-9 px-8 gap-x-4 lg:gap-x-6 min-h-screen">
+    <motion.div
+      animate={{ opacity: visible.value ? 1 : 0 }}
+      className="fixed inset-0 grid grid-cols-3 auto-rows-min md:grid-cols-6 lg:grid-cols-9 px-8 gap-x-4 lg:gap-x-6 min-h-screen"
+    >
       {[...Array(9)].map((_, i) => (
-        <div className="bg-black/5 h-screen" />
+        <div className="bg-black/5 h-screen" key={`toggle_grid__column-${i}`} />
       ))}
-    </div>
+    </motion.div>
   );
 };
 
