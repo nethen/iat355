a6718181581a9ba2e8a0454036b60c9cb1f31a4d
nethen <72537484+nethen@users.noreply.github.com>
2024-11-24 03:18:13 -0800

    fix: removed pointer events from grid overlay

--- src/app/components/Grid/Grid.tsx
+++ src/app/components/Grid/Grid.tsx
@@ -12,7 +12,7 @@ export const Grid = () => {
   return (
     <motion.div
       animate={{ opacity: visible.value ? 1 : 0 }}
-      className="fixed inset-0 grid-r"
+      className="fixed inset-0 grid-r pointer-events-none"
     >
       {[...Array(9)].map((_, i) => (
         <div className="bg-black/5 h-screen" key={`toggle_grid__column-${i}`} />
