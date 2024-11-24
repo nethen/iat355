e7189d613734242dacc323535bf045edebafb8be
nethen <72537484+nethen@users.noreply.github.com>
2024-11-24 03:08:01 -0800

    feat: generic ribbon nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Grid, GRID_STYLE } from "./components/Grid/Grid";
+import { Grid } from "./components/Grid/Grid";
 import clsx from "clsx";
 import Link from "next/link";
 
@@ -7,19 +7,14 @@ export default function Home() {
   return (
     <>
       <Grid />
-      <div
-        className={clsx(
-          GRID_STYLE,
-          "font-[family-name:var(--font-geist-sans)]"
-        )}
-      >
-        <nav className="sticky inset-y-0 md:h-svh flex flex-col">
+      <div className="grid-r font-[family-name:var(--font-geist-sans)]">
+        <nav className="fixed inset-x-0 max-md:grid-r md:sticky md:inset-y-0 md:h-svh flex flex-col bg-foreground text-background">
           <Link href="/">Prologue</Link>
           <Link href="/">Prologue</Link>
           <Link href="/">Prologue</Link>
           <Link href="/">Prologue</Link>
         </nav>
-        <main className="col-span-full md:col-span-5 lg:col-span-8 grid grid-cols-subgrid min-h-[200svh]">
+        <main className="col-span-full md:col-span-5 lg:col-span-8 grid grid-cols-subgrid auto-rows-min min-h-[200svh]">
           <h1 className="text-r-md">Population</h1>
           <p className="text-r-base">Act 1</p>
           <p className="text-r-base font-bold">Act 1</p>
