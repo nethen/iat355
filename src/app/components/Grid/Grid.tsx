0f60e7352431abd9210163762c2a765e70175303
nethen <72537484+nethen@users.noreply.github.com>
2024-11-24 03:07:52 -0800

    fix: component layer grid-r style

--- src/app/components/Grid/Grid.tsx
+++ src/app/components/Grid/Grid.tsx
@@ -12,7 +12,7 @@ export const Grid = () => {
   return (
     <motion.div
       animate={{ opacity: visible.value ? 1 : 0 }}
-      className="fixed inset-0 grid grid-cols-3 auto-rows-min md:grid-cols-6 lg:grid-cols-9 px-8 gap-x-4 lg:gap-x-6 min-h-screen"
+      className="fixed inset-0 grid-r"
     >
       {[...Array(9)].map((_, i) => (
         <div className="bg-black/5 h-screen" key={`toggle_grid__column-${i}`} />
@@ -20,6 +20,3 @@ export const Grid = () => {
     </motion.div>
   );
 };
-
-export const GRID_STYLE =
-  "grid auto-rows-min grid-cols-3 md:grid-cols-6 lg:grid-cols-9 px-8 gap-x-4 lg:gap-x-6 min-h-screen";
