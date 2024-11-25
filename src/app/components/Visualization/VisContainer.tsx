e347f42effca00baf354bd895ae84c632607dd2c
nethen <72537484+nethen@users.noreply.github.com>
2024-11-24 18:37:57 -0800

    fix: updated responsive vis container size

--- src/app/components/Visualization/VisContainer.tsx
+++ src/app/components/Visualization/VisContainer.tsx
@@ -1,5 +1,5 @@
 export const VisContainer = () => {
   return (
-    <figure className="aspect-square bg-foreground col-span-full lg:aspect-video lg:col-start-2 lg:col-span-5 rounded-lg"></figure>
+    <figure className="aspect-square bg-foreground col-span-full lg:aspect-video lg:col-span-7 2xl:col-start-2 2xl:col-span-5 rounded-lg"></figure>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,35 +31,45 @@ export default function Home() {
           </Link>
         </nav>
         <main className="max-md:pt-[72px] col-span-full py-[1.125em] md:py-[2.25em] md:col-span-5 lg:col-span-8 grid grid-cols-subgrid auto-rows-min min-h-[200svh] gap-y-[1.125em]">
-          <div className="col-span-full md:col-start-2 md:col-span-3 lg:col-start-3 lg:col-span-3 text-center">
-            <h2 className="text-r-md">Prologue</h2>
-          </div>
-          <VisContainer />
-          <p className="col-span-full md:col-start-2 md:col-span-3 lg:col-start-3 lg:col-span-3">
-            When considering the design schools around the Lower Mainland, SIAT
-            is a rare breed. With a jack of all trades mindset, students are
-            exposed to a variety of disciplines in their first and second years.
-          </p>
-          <p className="col-span-full md:col-start-2 md:col-span-3 lg:col-start-3 lg:col-span-3">
-            When considering the design schools around the Lower Mainland, SIAT
-            is a rare breed. With a jack of all trades mindset, students are
-            exposed to a variety of disciplines in their first and second years.
-          </p>
-          <div className="col-span-full md:col-start-2 md:col-span-3 lg:col-start-3 lg:col-span-3 text-center">
-            <span className="font-bold">Act I</span>
-            <h2 className="text-r-md">People</h2>
-          </div>
-          <VisContainer />
-          <div className="col-span-full md:col-start-2 md:col-span-3 lg:col-start-3 lg:col-span-3 text-center">
-            <span className="font-bold">Act II</span>
-            <h2 className="text-r-md">Program</h2>
-          </div>
-          <VisContainer />
-          <div className="col-span-full md:col-start-2 md:col-span-3 lg:col-start-3 lg:col-span-3 text-center">
-            <span className="font-bold">Act III</span>
-            <h2 className="text-r-md">Practice</h2>
-          </div>
-          <VisContainer />
+          <section className="col-span-full grid grid-cols-subgrid gap-[inherit] pb-[4.5em]">
+            <div className="col-span-full md:col-start-2 md:col-span-3 lg:col-start-3 lg:col-span-3 text-center">
+              <h2 className="text-r-md">Prologue</h2>
+            </div>
+            <VisContainer />
+            <p className="col-span-full md:col-start-2 md:col-span-3 lg:col-start-3 lg:col-span-3">
+              When considering the design schools around the Lower Mainland,
+              SIAT is a rare breed. With a jack of all trades mindset, students
+              are exposed to a variety of disciplines in their first and second
+              years.
+            </p>
+            <p className="col-span-full md:col-start-2 md:col-span-3 lg:col-start-3 lg:col-span-3">
+              When considering the design schools around the Lower Mainland,
+              SIAT is a rare breed. With a jack of all trades mindset, students
+              are exposed to a variety of disciplines in their first and second
+              years.
+            </p>
+          </section>
+          <section className="col-span-full grid grid-cols-subgrid gap-[inherit] pb-[4.5em]">
+            <div className="col-span-full md:col-start-2 md:col-span-3 lg:col-start-3 lg:col-span-3 text-center">
+              <span className="font-bold">Act I</span>
+              <h2 className="text-r-md">People</h2>
+            </div>
+            <VisContainer />
+          </section>
+          <section className="col-span-full grid grid-cols-subgrid gap-[inherit] pb-[4.5em]">
+            <div className="col-span-full md:col-start-2 md:col-span-3 lg:col-start-3 lg:col-span-3 text-center">
+              <span className="font-bold">Act II</span>
+              <h2 className="text-r-md">Program</h2>
+            </div>
+            <VisContainer />
+          </section>
+          <section className="col-span-full grid grid-cols-subgrid gap-[inherit] pb-[4.5em]">
+            <div className="col-span-full md:col-start-2 md:col-span-3 lg:col-start-3 lg:col-span-3 text-center">
+              <span className="font-bold">Act III</span>
+              <h2 className="text-r-md">Practice</h2>
+            </div>
+            <VisContainer />
+          </section>
         </main>
       </div>
     </ReactLenis>
