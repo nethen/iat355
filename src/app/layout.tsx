19e3078027d4cc949c1cdd12a546007448d9874e
nethen <72537484+nethen@users.noreply.github.com>
2024-11-26 03:40:31 -0800

    feat: opengraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,18 @@ export const metadata: Metadata = {
   title: "A Design Confidence Index",
   description:
     "A Design Confidence Index pulls insights from 69 polled undergraduate students from Simon Fraser University’s School of Interactive Arts and Technology (SIAT), asking the sample for insights into their perceptions and practices in visual design, creativity, and AI usage.",
+  openGraph: {
+    title: "A Design Confidence Index",
+    description:
+      "A Design Confidence Index pulls insights from 69 polled undergraduate students from Simon Fraser University’s School of Interactive Arts and Technology (SIAT), asking the sample for insights into their perceptions and practices in visual design, creativity, and AI usage.",
+    type: "website",
+    images: {
+      url: "/opengraph-image.png",
+      width: 1200,
+      height: 630,
+      alt: "A Design Confidence Index",
+    },
+  },
 };
 
 export default function RootLayout({
