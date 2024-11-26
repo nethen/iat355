621f1c7d4dad9b499e957c53020d2f8160e291e6
nethen <72537484+nethen@users.noreply.github.com>
2024-11-25 20:19:13 -0800

    fix: update diagram color

--- src/app/components/Visualization/VisContainer.tsx
+++ src/app/components/Visualization/VisContainer.tsx
@@ -1,5 +1,5 @@
 export const VisContainer = () => {
   return (
-    <figure className="aspect-square bg-foreground col-span-full lg:aspect-video lg:col-span-7 2xl:col-start-2 2xl:col-span-5 rounded-lg"></figure>
+    <figure className="aspect-square bg-diagram col-span-full lg:aspect-video lg:col-span-7 2xl:col-start-2 2xl:col-span-5 rounded-lg"></figure>
   );
 };
