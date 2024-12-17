1bbf9beb6d65093590a3accc11b22d436307ba7b
Michael Dresler <michaeldresler@hotmail.com>
2024-12-17 04:37:18 -0800

    removed magic numbers

--- src/app/components/D3/prologue/Participants.tsx
+++ src/app/components/D3/prologue/Participants.tsx
@@ -23,23 +23,22 @@ export const Participants = ({
   data,
   width = 640,
   height = 500,
-  // marginTop = 20,
-  marginRight = 20,
-  marginBottom = 20,
-  marginLeft = 20,
+  marginTop = 50,
+  marginBottom = 50,
+  marginRight = 50,
+  marginLeft = 50,
 }: D3VisProps) => {
   const scrollYProgress = useScrollYProgress();
   const hasCaption = useHasCaption();
   const scale = useTransform(scrollYProgress, [0.25, 0.75], [0, 6]);
 
-  //   const [extents, setExtents] = useState<number[] | undefined[]>([
-  //     undefined,
-  //     undefined,
-  //   ]);
-  const y = scaleLinear().domain([0, 1]).range([height, 40]); // Flip the range
-  const x = scaleLinear()
-    .domain([0, 35])
-    .range([marginLeft, width - marginRight]);
+  const filteredData = data?.filter((row) => row.isDesign === "1"); // Filter by inDesign column
+
+  const innerHeight = height - marginTop - marginBottom;
+  const innerWidth = width - marginLeft - marginRight;
+
+  const y = scaleLinear().domain([0, 1]).range([innerHeight, 0]); // Flip the range
+  const x = scaleLinear().domain([0, 35]).range([0, innerWidth]);
 
   console.log(x.ticks());
 
@@ -51,55 +50,69 @@ export const Participants = ({
     <motion.svg
       viewBox={`0 0 ${width} ${height}`}
       className="w-full h-auto"
-      animate={{ background: data ? (hasCaption ? "white" : "blue") : "red" }}
+      animate={{
+        background: filteredData ? (hasCaption ? "white" : "blue") : "red",
+      }}
     >
-      <g>
-        {y.ticks().map((tickValue) => (
-          <g transform={`translate(0, ${y(tickValue)})`} key={tickValue}>
-            <line
-              stroke="lightgrey"
-              x1={marginLeft + 40}
-              x2={width + 50}
-              y1={0}
-              y2={0}
-            />
-            <text
-              x={marginLeft + 20} // Adjust to position the text left of the axis
-              y={0} // Keep aligned with the tick
-              textAnchor="end" // Align text to the end of the position
-              style={{ fontSize: "1rem", fill: "black" }}
-            >
-              {tickValue * 100}
-            </text>
-          </g>
-        ))}
-      </g>
+      <g transform={`translate(${marginLeft},${marginTop})`}>
+        <g>
+          {y.ticks().map((tickValue) => (
+            <g transform={`translate(0, ${y(tickValue)})`} key={tickValue}>
+              <line
+                stroke="lightgrey"
+                x1={0}
+                x2={innerWidth}
+                y1={0}
+                y2={0}
+              />
+              <text
+                x={-5} // Adjust to position the text left of the axis
+                y={0} // Keep aligned with the tick
+                textAnchor="end" // Align text to the end of the position
+                style={{ fontSize: "1rem", fill: "black" }}
+              >
+                {`${tickValue * 100}%`}
+              </text>
+            </g>
+          ))}
+        </g>
 
-      <g>
-        {x.ticks().map((tickValue) => (
-          <g transform={`translate(${x(tickValue)},0)`} key={tickValue}>
-            <line stroke="lightgrey" x={marginLeft} y1={0} y2={0} />
-            <text
-              x={marginLeft + 20} // Adjust to position the text left of the axis
-              y={height + marginBottom} // Keep aligned with the tick
-              textAnchor="end" // Align text to the end of the position
-              style={{ fontSize: "1rem", fill: "black" }}
-            >
-              {tickValue}
-            </text>
-          </g>
-        ))}
-      </g>
-      {data &&
-        data.map((d, i) => (
+        <g>
+          {x.ticks().map((tickValue) => (
+            <g transform={`translate(${x(tickValue)},0)`} key={tickValue}>
+              <line
+                stroke="lightgrey"
+                x1={0}
+                x2={0}
+                y1={0}
+                y2={innerHeight}
+              />
+              <line stroke="lightgrey" x={0} y1={0} y2={0} />
+              <text
+                x={0} // Adjust to position the text left of the axis
+                y={innerHeight + 20} // Keep aligned with the tick
+                textAnchor="end" // Align text to the end of the position
+                style={{ fontSize: "1rem", fill: "black", textAlign: "right" }}
+              >
+                {tickValue}
+              </text>
+            </g>
+          ))}
+        </g>
+
+
+      {filteredData &&
+        filteredData.map((d, i) => (
           <motion.circle
             cx={x(parseFloat(d.visual_confidence_score))}
             cy={y(parseFloat(d.score_percent))}
-            fill="#000422"
+            fill="#1058c4"
             r={scale}
             key={`participant-${i}`}
           />
         ))}
+
+</g>
     </motion.svg>
   );
 };
